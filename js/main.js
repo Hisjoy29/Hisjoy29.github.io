c092173793a43d8c4c7449426ca4db6cb31a9421
가인지 <gainge@gainjiui-MacBookPro-2.local>
2022-09-12 23:07:51 +0900

    add phone and sms function

--- js/main.js
+++ js/main.js
@@ -14,3 +14,43 @@ const observer = new IntersectionObserver(entries => {
 const fadeInDivs = document.querySelectorAll(".fadeIn")
 
 fadeInDivs.forEach(fade => observer.observe(fade))
+
+function call_to_groom()
+{
+  return document.location.href = "tel:010-8253-1206"
+}
+
+function call_to_bride()
+{
+  return document.location.href = "tel:010-3371-5003"
+}
+
+function call_to_groom_father()
+{
+  return document.location.href = "tel:010-2809-0317"
+}
+
+function call_to_groom_mother()
+{
+  return document.location.href = "tel:010-4408-1132"
+}
+
+function call_to_bride_father()
+{
+  return document.location.href = "tel:010-1234-5678"
+}
+
+function call_to_bride_mother()
+{
+  return document.location.href = "tel:010-1234-5678"
+}
+
+function message_to_groom()
+{
+  return document.location.href = "sms:010-8253-1206"
+}
+
+function message_to_bride()
+{
+  return document.location.href = "sms:010-3371-5003"
+}
